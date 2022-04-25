97b5fe17776070e3484833a427595f20a89b92b1
Fahim <fahim.madani@gmail.com>
2022-04-26 00:46:12 +0600

    created genius car server

---
+++ __server_deploy.js
@@ -0,0 +1,10 @@
+/*
+create heroky account 
+verify email 
+install heroku cli 
+heroku login
+---------------------------------
+for each project 
+---------------------------------
+create a heroku project
+*/
